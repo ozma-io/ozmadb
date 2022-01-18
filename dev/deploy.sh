25fba9eae3957910e7b6c8c5224f9e880d90f33a
Nikolay Amiantov <ab@fmap.me>
2022-01-18 21:31:35 +0000

    Cleanup deploy

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 usage() {
-  echo "Usage: $0 server_name deployment_name network_path" >&2
+  echo "Usage: $0 server_name deployment_name" >&2
   exit 1
 }
 
@@ -13,10 +13,7 @@ shift
 deployment_name="$1"
 shift
 
-network_path="$1"
-shift
-
-if [ -z "$server_name" ] || [ -z "$deployment_name" ] || [ -z "$network_path" ]; then
+if [ -z "$server_name" ] || [ -z "$deployment_name" ]; then
   usage
 fi
 
@@ -25,4 +22,4 @@ set -x
 rsync -ravlL --delete FunDB/publish/ "$server_name:$deployment_name/FunDB"
 rsync -ravlL --delete FunApp/dist/ "$server_name:$deployment_name/FunApp"
 
-ssh "$server_name" -- nixops deploy --network "$network_path"
+ssh "$server_name" -- nixops deploy --network "$deployment_name/machines"
