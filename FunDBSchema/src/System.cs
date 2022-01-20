23c0b773126b4a003b55c3088789a87a57b00f56
Nikolay Amiantov <ab@fmap.me>
2022-01-20 17:13:48 +0000

    Drop parents column

--- FunDBSchema/src/System.cs
+++ FunDBSchema/src/System.cs
@@ -419,7 +419,6 @@ namespace FunWithFlags.FunDBSchema.System
         public string Name { get; set; } = null!;
         [ColumnField("bool", Default="false")]
         public bool AllowBroken { get; set; }
-        [ColumnField("bool", Default="false")]
 
         public List<RoleParent>? Parents { get; set; }
         public List<RoleParent>? Children { get; set; }
