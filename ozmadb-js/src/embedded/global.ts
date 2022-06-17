c73e1314f0763d6cabef51d99647becbc4bef783
Nikolay Amiantov <ab@fmap.me>
2022-06-17 10:19:57 +0000

    Use window

--- ozmadb-js/src/embedded/global.ts
+++ ozmadb-js/src/embedded/global.ts
@@ -6,5 +6,6 @@ declare global {
   var FunApp: typeof Index.FunApp;
   var FunAppEmbeddedError: typeof Index.FunAppEmbeddedError;
 }
-global.FunApp = Index.FunApp;
-global.FunAppEmbeddedError = Index.FunAppEmbeddedError;
+
+window.FunApp = Index.FunApp;
+window.FunAppEmbeddedError = Index.FunAppEmbeddedError;
