481dd8f388678461a1d1181af4e2e168946c3585
Nikolay Amiantov <ab@fmap.me>
2023-08-15 04:16:37 +0000

    Make token an internal state of the client

--- ozmadb-js/src/client/client.ts
+++ ozmadb-js/src/client/client.ts
@@ -207,24 +207,46 @@ const commonHeaders = {
   "X-FunDB-LongRunning": "hybrid",
 };
 
-export default class FunDBClient {
-  private apiUrl: string;
+interface IFunDBRequestInit {
+  method?: string;
+  headers?: Record<string, string>;
+  body?: Blob | string;
+}
 
-  constructor(opts: { apiUrl: string }) {
+export default class FunDBClient {
+  apiUrl: string;
+  token: null | string;
+
+  constructor(opts: { apiUrl: string; token?: string }) {
     this.apiUrl = opts.apiUrl;
+    this.token = opts.token ?? null;
   }
 
-  private async fetchFunDB(input: RequestInfo, init?: RequestInit): Promise<Response> {
-    const response = await fetchRawFunDB(input, init);
+  private async fetchFunDB(url: string, opts?: IFunDBRequestInit): Promise<Response> {
+    const headers: Record<string, string> = {
+      ...commonHeaders,
+      ...opts?.headers,
+    };
+    if (this.token !== null) {
+      headers["Authorization"] = `Bearer ${this.token}`;
+    }
+    const response = await fetchRawFunDB(url, { ...opts, headers });
     if (!response.ok) {
       const body = await response.json() as ClientRawApiError;
       if (body.error === "notFinished") {
         let jobId = body.id;
         while (true) {
+          // Update the token.
+          if (this.token !== null) {
+            headers["Authorization"] = `Bearer ${this.token}`;
+          } else {
+            delete headers["Authorization"];
+          }
           // eslint-disable-next-line no-await-in-loop
-          const newResponse = await this.fetchFunDB(`${this.apiUrl}/jobs/${jobId}/result`, {
-            headers: init?.headers ?? {},
-          });
+          const newResponse = await this.fetchFunDB(
+            `${this.apiUrl}/jobs/${jobId}/result`,
+            { ...opts, headers },
+          );
           if (!newResponse.ok) {
             // eslint-disable-next-line no-await-in-loop
             const newBody = await response.json() as ClientRawApiError;
@@ -245,136 +267,121 @@ export default class FunDBClient {
     }
   }
 
-  private async fetchJson(input: RequestInfo, init?: RequestInit): Promise<unknown> {
-    const response = await this.fetchFunDB(input, init);
+  private async fetchJson(url: string, opts?: IFunDBRequestInit): Promise<unknown> {
+    const response = await this.fetchFunDB(url, opts);
     return response.json();
   }
 
-  private async fetchGetFileApi(subUrl: string, token: string | null, accept: string): Promise<Blob> {
-    const headers: Record<string, string> = {
-      ...commonHeaders,
-    };
-    if (token !== null) {
-      headers["Authorization"] = `Bearer ${token}`;
-    }
-    headers["Accept"] = accept;
+  private async fetchGetFileApi(subUrl: string, accept: string): Promise<Blob> {
     const response = await this.fetchFunDB(`${this.apiUrl}/${subUrl}`, {
       method: "GET",
-      headers,
+      headers: {
+        "Accept": accept,
+      },
     });
     return response.blob();
   }
 
-  private async fetchJsonApi(subUrl: string, token: string | null, method: string, body?: unknown): Promise<unknown> {
-    const headers: Record<string, string> = {
-      ...commonHeaders,
-      "Content-Type": "application/json",
-    };
-    if (token !== null) {
-      headers["Authorization"] = `Bearer ${token}`;
-    }
+  private async fetchJsonApi(subUrl: string, method: string, body?: unknown): Promise<unknown> {
     return this.fetchJson(`${this.apiUrl}/${subUrl}`, {
       method,
-      headers,
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify(body),
     });
   }
 
-  private async fetchSendFileApi(subUrl: string, token: string | null, method: string, contentType: string, body: Blob): Promise<unknown> {
-    const headers: Record<string, string> = {
-      ...commonHeaders,
-      "Content-Type": contentType,
-    };
-    if (token !== null) {
-      headers["Authorization"] = `Bearer ${token}`;
-    }
+  private async fetchSendFileApi(subUrl: string, method: string, contentType: string, body: Blob): Promise<unknown> {
     return this.fetchJson(`${this.apiUrl}/${subUrl}`, {
       method,
-      headers,
+      headers: {
+        "Content-Type": contentType,
+      },
       body,
     });
   }
 
   // We use `POST` method to ensure we won't bloat URLs and to have full access to Chunk APIs.
-  private getUserView = async (path: string, token: string | null, body: any): Promise<IViewExprResult> => {
-    return this.fetchJsonApi(`views/${path}/entries`, token, "POST", body) as Promise<IViewExprResult>;
+  private getUserView = async (path: string, body: any): Promise<IViewExprResult> => {
+    return this.fetchJsonApi(`views/${path}/entries`, "POST", body) as Promise<IViewExprResult>;
   };
 
-  private getUserViewInfo = async (path: string, token: string | null, body: any): Promise<IViewInfoResult> => {
-    return this.fetchJsonApi(`views/${path}/info`, token, "POST", body) as Promise<IViewInfoResult>;
+  private getUserViewInfo = async (path: string, body: any): Promise<IViewInfoResult> => {
+    return this.fetchJsonApi(`views/${path}/info`, "POST", body) as Promise<IViewInfoResult>;
   };
 
-  private getUserViewExplain = async (path: string, token: string | null, body: any): Promise<IViewExplainResult> => {
-    return this.fetchJsonApi(`views/${path}/explain`, token, "POST", body) as Promise<IViewExplainResult>;
+  private getUserViewExplain = async (path: string, body: any): Promise<IViewExplainResult> => {
+    return this.fetchJsonApi(`views/${path}/explain`, "POST", body) as Promise<IViewExplainResult>;
   };
 
-  getAnonymousUserView = async (token: string | null, query: string, args?: Record<string, unknown>, opts?: IEntriesRequestOpts): Promise<IViewExprResult> => {
+  getAnonymousUserView = async (query: string, args?: Record<string, unknown>, opts?: IEntriesRequestOpts): Promise<IViewExprResult> => {
     const req: IAnonymousUserViewEntriesRequest = {
       ...opts,
       query,
       args,
     };
-    return this.getUserView("anonymous", token, req);
+    return this.getUserView("anonymous", req);
   };
 
-  getNamedUserView = async (token: string | null, ref: IUserViewRef, args?: Record<string, unknown>, opts?: IEntriesRequestOpts): Promise<IViewExprResult> => {
+  getNamedUserView = async (ref: IUserViewRef, args?: Record<string, unknown>, opts?: IEntriesRequestOpts): Promise<IViewExprResult> => {
     const req: IUserViewEntriesRequest = {
       ...opts,
       args,
     };
-    return this.getUserView(`by_name/${ref.schema}/${ref.name}`, token, req);
+    return this.getUserView(`by_name/${ref.schema}/${ref.name}`, req);
   };
 
-  getNamedUserViewInfo = async (token: string | null, ref: IUserViewRef, opts?: IInfoRequestOpts): Promise<IViewInfoResult> => {
-    return this.getUserViewInfo(`by_name/${ref.schema}/${ref.name}`, token, opts);
+  getNamedUserViewInfo = async (ref: IUserViewRef, opts?: IInfoRequestOpts): Promise<IViewInfoResult> => {
+    return this.getUserViewInfo(`by_name/${ref.schema}/${ref.name}`, opts);
   };
 
-  getAnonymousUserViewExplain = async (token: string | null, query: string, args?: Record<string, unknown>, opts?: IEntriesExplainOpts): Promise<IViewExplainResult> => {
+  getAnonymousUserViewExplain = async (query: string, args?: Record<string, unknown>, opts?: IEntriesExplainOpts): Promise<IViewExplainResult> => {
     const req: IAnonymousUserViewExplainRequest = {
       ...opts,
       query,
       args,
     };
-    return this.getUserViewExplain("anonymous", token, req);
+    return this.getUserViewExplain("anonymous", req);
   };
 
-  getNamedUserViewExplain = async (token: string | null, ref: IUserViewRef, args?: Record<string, unknown>, opts?: IEntriesExplainOpts): Promise<IViewExplainResult> => {
+  getNamedUserViewExplain = async (ref: IUserViewRef, args?: Record<string, unknown>, opts?: IEntriesExplainOpts): Promise<IViewExplainResult> => {
     const req: IUserViewExplainRequest = {
       ...opts,
       args,
     };
-    return this.getUserViewExplain(`by_name/${ref.schema}/${ref.name}`, token, req);
+    return this.getUserViewExplain(`by_name/${ref.schema}/${ref.name}`, req);
   };
 
-  getEntityInfo = async (token: string | null, ref: IEntityRef): Promise<IEntity> => {
-    return this.fetchJsonApi(`entities/${ref.schema}/${ref.name}/info`, token, "GET") as Promise<IEntity>;
+  getEntityInfo = async (ref: IEntityRef): Promise<IEntity> => {
+    return this.fetchJsonApi(`entities/${ref.schema}/${ref.name}/info`, "GET") as Promise<IEntity>;
   };
 
-  runTransaction = async (token: string | null, action: ITransaction): Promise<ITransactionResult> => {
-    return this.fetchJsonApi("transaction", token, "POST", action) as Promise<ITransactionResult>;
+  runTransaction = async (action: ITransaction): Promise<ITransactionResult> => {
+    return this.fetchJsonApi("transaction", "POST", action) as Promise<ITransactionResult>;
   };
 
-  runAction = async (token: string | null, ref: IActionRef, args?: Record<string, unknown>): Promise<IActionResult> => {
-    return this.fetchJsonApi(`actions/${ref.schema}/${ref.name}/run`, token, "POST", args ?? {}) as Promise<IActionResult>;
+  runAction = async (ref: IActionRef, args?: Record<string, unknown>): Promise<IActionResult> => {
+    return this.fetchJsonApi(`actions/${ref.schema}/${ref.name}/run`, "POST", args ?? {}) as Promise<IActionResult>;
   };
 
-  getDomainValues = async (token: string | null, ref: IFieldRef, rowId?: number, opts?: IEntriesRequestOpts): Promise<IDomainValuesResult> => {
+  getDomainValues = async (ref: IFieldRef, rowId?: number, opts?: IEntriesRequestOpts): Promise<IDomainValuesResult> => {
     const req: IDomainsRequest = {
       ...opts,
       rowId,
     };
-    return this.fetchJsonApi(`domains/${ref.entity.schema}/${ref.entity.name}/${ref.name}/entries`, token, "POST", req) as Promise<IDomainValuesResult>;
+    return this.fetchJsonApi(`domains/${ref.entity.schema}/${ref.entity.name}/${ref.name}/entries`, "POST", req) as Promise<IDomainValuesResult>;
   };
 
-  getDomainExplain = async (token: string | null, ref: IFieldRef, rowId?: number, opts?: IEntriesExplainOpts): Promise<IExplainedQuery> => {
+  getDomainExplain = async (ref: IFieldRef, rowId?: number, opts?: IEntriesExplainOpts): Promise<IExplainedQuery> => {
     const req: IDomainsExplainRequest = {
       ...opts,
       rowId,
     };
-    return this.fetchJsonApi(`domains/${ref.entity.schema}/${ref.entity.name}/${ref.name}/explain`, token, "POST", req) as Promise<IExplainedQuery>;
+    return this.fetchJsonApi(`domains/${ref.entity.schema}/${ref.entity.name}/${ref.name}/explain`, "POST", req) as Promise<IExplainedQuery>;
   };
 
-  saveSchemas = async (token: string | null, schemas: string[] | "all", options?: ISaveSchemasOptions): Promise<Blob> => {
+  saveSchemas = async (schemas: string[] | "all", options?: ISaveSchemasOptions): Promise<Blob> => {
     const params = new URLSearchParams();
     if (schemas !== "all") {
       schemas.forEach(name => {
@@ -384,10 +391,10 @@ export default class FunDBClient {
     if (options?.skipPreloaded) {
       params.append("skip_preloaded", "true");
     }
-    return this.fetchGetFileApi(`layouts?${params}`, token, "application/zip");
+    return this.fetchGetFileApi(`layouts?${params}`, "application/zip");
   };
 
-  restoreSchemas = async (token: string | null, data: Blob, options?: IRestoreSchemasOptions): Promise<void> => {
+  restoreSchemas = async (data: Blob, options?: IRestoreSchemasOptions): Promise<void> => {
     const params = new URLSearchParams();
     if (options?.dropOthers) {
       params.append("drop_others", "true");
@@ -395,10 +402,10 @@ export default class FunDBClient {
     if (options?.forceAllowBroken) {
       params.append("force_allow_broken", "true");
     }
-    await this.fetchSendFileApi(`layouts?${params}`, token, "PUT", "application/zip", data);
+    await this.fetchSendFileApi(`layouts?${params}`, "PUT", "application/zip", data);
   };
 
   getPermissions = async (token: string | null): Promise<IPermissionsInfo> => {
-    return this.fetchJsonApi("permissions", token, "GET") as Promise<IPermissionsInfo>;
+    return this.fetchJsonApi("permissions", "GET") as Promise<IPermissionsInfo>;
   };
 }
