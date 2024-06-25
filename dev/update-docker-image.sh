ce01efd43dff522eb434c6d7da43f45cdc593372
Nikolay Amiantov <ab@fmap.me>
2024-06-26 02:32:29 +0700

    Remove the obsolete Docker image

--- dev/update-docker-image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-# Build and upload new version of development images.
-
-docker build --no-cache -t myprocessx/funwithflags-dotnet:latest -f dev/Dockerfile.dotnet dev
-docker push myprocessx/funwithflags-dotnet:latest
