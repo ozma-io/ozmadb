fdd434c09cf2d17d2cf6313c0367ed6c3cf9409f
Nikolay Amiantov <ab@fmap.me>
2024-06-27 10:08:47 +0000

    More JS fixes

--- ozmadb-js/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Legacy entry point.
-
-import FunDBClient from "./client";
-
-export * from "./client";
-export * from "./embedded/utils";
-export * as Embedded from "./embedded/types";
-export default FunDBClient;
