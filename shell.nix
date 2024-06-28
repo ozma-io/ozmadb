14fd2e4c73d3bf0b496fe00f6a796efac09ec21b
Nikolay Amiantov <ab@fmap.me>
2024-06-28 11:24:59 +0000

    Format Nix

--- shell.nix
+++ shell.nix
@@ -1,32 +1,32 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-let
+{pkgs ? import <nixpkgs> {}}: let
   dotnet = pkgs.dotnet-sdk_8;
   nodejs = pkgs.nodejs_22;
   python = pkgs.python3;
 
   env = pkgs.buildFHSUserEnv {
     name = "ozmadb";
-    targetPkgs = pkgs: with pkgs; [
-      # OzmaDB
-      dotnet
-      zlib
-      mono
-      lttng-ust
-      libunwind
-      kerberos
-      lldb
-      openssl
-      # ozmadb-js
-      nodejs
-      yarn
-      # ozmadb-py
-      poetry
-      python
-      ruff
-      pyright
-    ];
-    extraOutputsToInstall = [ "dev" ];
+    targetPkgs = pkgs:
+      with pkgs; [
+        alejandra
+        # OzmaDB
+        dotnet
+        zlib
+        mono
+        lttng-ust
+        libunwind
+        kerberos
+        lldb
+        openssl
+        # ozmadb-js
+        nodejs
+        yarn
+        # ozmadb-py
+        poetry
+        python
+        ruff
+        pyright
+      ];
+    extraOutputsToInstall = ["dev"];
     profile = ''
       export DOTNET_ROOT=${dotnet}
     '';
@@ -37,12 +37,12 @@ let
   };
 
   userShell = builtins.getEnv "SHELL";
+in
+  pkgs.stdenv.mkDerivation {
+    name = "ozmadb-fhs-dev";
 
-in pkgs.stdenv.mkDerivation {
-  name = "ozmadb-fhs-dev";
-
-  shellHook = ''
-    exec ${env}/bin/ozmadb
-  '';
-  buildCommand = "exit 1";
-}
+    shellHook = ''
+      exec ${env}/bin/ozmadb
+    '';
+    buildCommand = "exit 1";
+  }
